9662ed82843a75d83c7c37e438237b6d6aed095d
Anton Tkachuk <tkachuk.ant@mail.ru>
2024-02-13 13:06:39 +0300

    added store provider to entry point

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {
   RouterProvider,
+} from 'react-router-dom'
+import { router, } from './app/router'
+import { store, } from './app/store/store'
+import { Provider, } from 'react-redux'
 import './app/styles/global.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={ router } />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <RouterProvider router={ router } />
+    </Provider>
+  </React.StrictMode>
 )

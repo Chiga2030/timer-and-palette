7b4196e20b307fbe66bd031329b3895fe2120801
Anton Tkachuk <tkachuk.ant@mail.ru>
2024-02-13 20:22:15 +0300

    change router to hash router

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter, } from 'react-router-dom'
+import { createHashRouter, } from 'react-router-dom'
 import { MainPage, } from '../pages/MainPage/MainPage'
 import { PalettePage, } from '../pages/PalettePage/PalettePage'
 import { MainContainer, } from './MainContainer/MainContainer'
 
 
-export const router = createBrowserRouter([ {
+export const router = createHashRouter([ {
   path: '/',
   element: <MainContainer><MainPage /></MainContainer>,
 }, {
